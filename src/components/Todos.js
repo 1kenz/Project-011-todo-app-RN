47772ccd8e75a19dbdc7f99ebbdea8a9e2411ade
xkendx <63884610+xkendx@users.noreply.github.com>
2020-10-15 03:48:33 +0300

    Create Todos.js

---
+++ src/components/Todos.js
@@ -0,0 +1,18 @@
+/* eslint-disable quotes */
+/* eslint-disable react-native/no-inline-styles */
+import React, { useState } from "react";
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  TextInput,
+  TouchableOpacity,
+  FlatList,
+  KeyboardAvoidingView,
+} from "react-native";
+
+
+export default function TodoItem
