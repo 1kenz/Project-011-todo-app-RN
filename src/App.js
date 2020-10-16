bc97927a6138b636a3889bb099aecfbb3510d5f1
xkendx <63884610+xkendx@users.noreply.github.com>
2020-10-16 04:10:19 +0300

    finalize

--- src/App.js
+++ src/App.js
@@ -1,100 +1,63 @@
-/* eslint-disable quotes */
-/* eslint-disable react-native/no-inline-styles */
-import React, { useState } from "react";
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-  TextInput,
-  TouchableOpacity,
-} from "react-native";
-
-import MyBanner from "./components/Header";
-
-const todos = [
-  {
-    id: 0,
-    todo: "Ev temizlenecek",
-    isDone: false,
-  },
-  {
-    id: 1,
-    todo: "Alışveriş yapılacak",
-    isDone: false,
-  },
-  {
-    id: 2,
-    todo: "Araba yıkanacak",
-    isDone: false,
-  },
-];
+import React, { useState, useEffect } from "react";
+import { View, Text, FlatList, StyleSheet, Alert } from "react-native";
+import TodoItem from "./components/Todos";
+import Header from "./components/Header";
+import AddTodo from "./components/addTodo";
 
 const App = () => {
+  const [todos, setTodos] = useState([]);
+  const todo_num = todos.length;
 
-  var todo_num = todos.length;
+  const pressHandler = (key) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todos) => todos.key != key);
+    });
+  };
 
+  const submitHandler = (text) => {
+    if (text.length < 3) {
+      Alert.alert("Lighthouse-Team Todo App", "Please enter todo!!")
+    }
+    else {
+      setTodos((prevTodos) => {
+        return [
+          { key: todos.length, todo: text, isDone: false },
+          ...prevTodos
+        ];
+      })
+    }
+  }
+
+  useEffect(() => {
+    Alert.alert("Lighthouse-Team", "Welcome to our todo app!")
+  }, [])
 
   return (
-    <SafeAreaView style={[styles.container, { flex: 1 }]}>
-      <View style={{ flexDirection: "row" }}>
-        <Text style={[styles.header, { flex: 1 }]}>TODO</Text>
-        <Text style={[styles.todo_count]}>{todo_num}</Text>
-      </View>
-      <View style={{ flex: 7 }}>
-        {todos.map((item) => {
-          return (<MyBanner todo={item.todo}
-          // isDone={item.isDone} 
-          />)
-        })}
-      </View>
-
-      <View style={{ flex: 2 }}>
-        <View style={styles.bottom_container}>
-          <TextInput style={styles.textInput} />
-          <TouchableOpacity
-            style={styles.todo_add_button}>
-            <Text style={styles.button_text}>ADD TODO</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <Header item={todo_num} />
+      <AddTodo submitHandler={submitHandler} />
+      <FlatList
+        data={todos}
+        renderItem={({ item }) => (
+          <TodoItem item={item} pressHandler={pressHandler} />
+        )}
+      />
+    </View>
   );
 };
 
+export default App;
+
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "#37474F",
+    backgroundColor: "#1e272e",
+    flex: 1,
   },
-  header: {
-    fontSize: 40,
-    fontWeight: "bold",
-    color: "orange",
-    margin: 5,
-    // backgroundColor:"red"
-  },
-  todo_count: {
-    fontSize: 30,
-    color: "orange",
-    alignSelf: "center",
-    margin: 10,
-    // backgroundColor:"yellow"
-  },
-  todos: {
-    textAlign: "center",
-    justifyContent: "center",
-    backgroundColor: "#CFD8DC",
-    borderRadius: 5,
-    margin: 10,
-    padding: 7,
-    color: "black"
-  },
+
   bottom_container: {
     textAlign: "center",
     justifyContent: "center",
-    backgroundColor: "#CFD8DC",
+    backgroundColor: "#e2e2e2",
     borderRadius: 15,
     margin: 10,
   },
@@ -110,7 +73,7 @@ const styles = StyleSheet.create({
     width: 200,
     height: 40,
     borderRadius: 5,
-    backgroundColor: "#37474F",
+    backgroundColor: "#e2e2e2",
     fontSize: 15,
     fontWeight: "bold",
     alignSelf: "center",
@@ -123,6 +86,33 @@ const styles = StyleSheet.create({
     color: "white",
     textAlign: "center",
   },
+  todo: {
+    // textAlign: "center",
+    justifyContent: "center",
+    backgroundColor: "#CFD8DC",
+    borderRadius: 5,
+    margin: 10,
+    padding: 10,
+    color: "black",
+    fontWeight: "bold",
+  },
+  delete: {
+    // textAlign: "center",
+    justifyContent: "center",
+    backgroundColor: "red",
+    borderRadius: 5,
+    margin: 10,
+    padding: 10,
+    color: "white",
+    fontWeight: "bold",
+  },
+  isDone: {
+    justifyContent: "center",
+    backgroundColor: "#CFD8DC",
+    borderRadius: 5,
+    margin: 10,
+    padding: 5,
+    color: "black",
+    textDecorationLine: "line-through",
+  },
 });
-
-export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,37 +1,43 @@
 import React from "react";
 import {
-  SafeAreaView,
   StyleSheet,
-  ScrollView,
   View,
   Text,
-  StatusBar,
-  TextInput,
-  TouchableOpacity,
+  Image
 } from "react-native";
 
-const Header = (props) => {
+export default function Header({ item }) {
   return (
-    <View style={{ flexDirection: "row" }}>
-      <Text style={[styles.header, { flex: 1 }]}>TODO</Text>
-      <Text style={[styles.todo_count]}>{props.num}</Text>
+    <View style={styles.header}>
+      <Image style={styles.image} source={require('../img/2.png')} />
+      <Text style={[styles.header,]}>My Todos</Text>
+      <Text style={styles.todo_count}>{item}</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   header: {
-    fontSize: 40,
+    fontSize: 35,
     fontWeight: "bold",
-    color: "orange",
+    color: "#ffa801",
     margin: 5,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
   },
   todo_count: {
-    fontSize: 30,
-    color: "orange",
+    fontSize: 35,
+    color: "#e2e2e2",
     alignSelf: "center",
+    fontWeight: "bold",
     margin: 10,
   },
-});
+  image: {
+    width: 50,
+    height: 50,
+    marginTop: 10,
+    marginLeft: 10,
 
-export default Header;
+  }
+});
